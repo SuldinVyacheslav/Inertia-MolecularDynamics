d3c8bc13fedf095682858721005366304219d0d6
Suldin Vyacheslav <suldin_slava.03@mail.ru>
2022-12-03 14:48:30 +0300

    Added new representation

--- template/Graph.cpp
+++ template/Graph.cpp
@@ -11,19 +11,23 @@ std::vector<double> values;
 
 void Graph::update_graph(sf::RenderWindow *window, double new_value)
 {
+    top = 0;
     if (values.size() < X)
     {
         values.push_back(new_value);
+        top += new_value;
     }
     else
     {
         for (int i = 0; i < values.size(); i++)
         {
             values[i] = values[i + 1];
+            top += values[i];
         }
         values[values.size()] = new_value;
+        top += new_value;
     }
-    top = new_value > top ? new_value : top;
+    top /= X;
     for (int i = values.size(); i > 1; i--)
     {
         draw(window, 2, color,
